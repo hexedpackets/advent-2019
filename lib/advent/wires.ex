b934ea2d32e074e5c6719544813501b51c9b6fc7
William Huba <hexedpackets@gmail.com>
2019-12-09 09:57:03 -0500

    Calculate shortest path for wires.

--- lib/advent/wires.ex
+++ lib/advent/wires.ex
@@ -2,7 +2,40 @@ defmodule Advent.Wires do
   def solve() do
     Advent.input()
     |> String.split()
-    |> closest_crossing_distance()
+    |> fewest_steps()
+  end
+
+  @doc """
+  ## Examples
+
+      iex> fewest_steps(["R8,U5,L5,D3", "U7,R6,D4,L4"])
+      30
+
+      iex> fewest_steps(["R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83"])
+      610
+  """
+  def fewest_steps(inputs) do
+    [wire1, wire2] = wire_mapping(inputs)
+
+    find_crosses(wire1, wire2)
+    |> Enum.filter(fn x -> x != {0, 0} end)
+    |> Enum.map(fn crossing ->
+      d1 = wire1 |> Enum.take_while(fn x -> x != crossing end) |> distance_travelled()
+      d2 = wire2 |> Enum.take_while(fn x -> x != crossing end) |> distance_travelled()
+      d1 + d2
+    end)
+    |> Enum.sort()
+    |> List.first()
+  end
+
+  def distance_travelled(coordinates) do
+    coordinates
+    |> Enum.reduce({0, 0, 0}, fn {x, y}, {total, last_x, last_y} ->
+      total = total + abs(x - last_x) + abs(y - last_y)
+      {total, x, y}
+    end)
+    |> elem(0)
+    |> Kernel.+(1)
   end
 
   @doc """
@@ -15,8 +48,9 @@ defmodule Advent.Wires do
       135
   """
   def closest_crossing_distance(inputs) do
-    inputs
-    |> find_crosses()
+    [wire1, wire2] = wire_mapping(inputs)
+
+    find_crosses(wire1, wire2)
     |> Stream.map(&manhattan_distance/1)
     |> Stream.filter(fn x -> x != 0 end)
     |> Enum.sort()
@@ -26,23 +60,27 @@ defmodule Advent.Wires do
   @doc """
   ## Examples
 
-      iex> find_crosses(["R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83"])
+      iex> [wire1, wire2] = wire_mapping(["R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83"])
+      ...> find_crosses(wire1, wire2)
       ...> |> Enum.map(&manhattan_distance/1)
       [0, 192, 159, 166, 170]
   """
   def manhattan_distance({x, y}), do: abs(x) + abs(y)
 
-  def find_crosses(inputs) do
-    [wire1, wire2] =
-      inputs
-      |> Enum.map(fn path ->
-        path
-        |> String.split(",")
-        |> grid_positions()
-        |> MapSet.new()
-      end)
+  def find_crosses(wire1, wire2) do
+    wire2 = MapSet.new(wire2)
 
-    Enum.filter(wire1, fn pos -> MapSet.member?(wire2, pos) end)
+    wire1
+    |> MapSet.new()
+    |> Enum.filter(fn pos -> MapSet.member?(wire2, pos) end)
+  end
+
+  def wire_mapping(inputs) do
+    Enum.map(inputs, fn path ->
+      path
+      |> String.split(",")
+      |> grid_positions()
+    end)
   end
 
   def grid_positions(moves), do: grid_positions(moves, [{0,0}])
